4997be883fb2c0af4599ebc38c23720e17388a9f
patrick <patrick@patrickdeMacBook-Pro.local>
2016-02-29 08:52:45 +0800

    update site

--- deploy_gh.sh
+++ deploy_gh.sh
@@ -1,1 +1,13 @@
 echo "Generating site content......"
+
+# git subtree add --prefix=public git@github.com:simonpatrick/simonpatrick.github.io.git gh-pages --squash
+# git subtree pull --prefix=public git@github.com:simonpatrick/simonpatrick.github.io.git gh-pages
+hugo -t robust
+
+# Add everything
+git add -A
+
+# Commit and push to master
+git commit -m "$1" && git push origin master
+
+git subtree push --prefix=public git@github.com:simonpatrick/simonpatrick.github.io.git gh-pages
