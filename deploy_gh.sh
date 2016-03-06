607c32310ac9aecf6d59ac04c788f4c0aac49139
patrick <patrick@patrickdeMacBook-Pro.local>
2016-03-06 16:41:52 +0800

    update deploy_gh.sh

--- deploy_gh.sh
+++ deploy_gh.sh
@@ -11,3 +11,8 @@ git add -A
 git commit -m "$1" && git push origin master
 
 git subtree push --prefix=public git@github.com:simonpatrick/simonpatrick.github.io.git gh-pages
+
+cp -rf public/* ../testless.github.io/
+cd ../testless.github.io/
+git add .
+git commit -m "$1" && git push 
